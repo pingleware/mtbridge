c788b7063594e7a2fac19f76e54111d422e592e7
PIPNGLEWARE <maintainers@pingleware.work>
2024-01-02 14:03:22 -0500

    package.json build

--- build.cmd
+++ build.cmd
@@ -1,10 +1,7 @@
-del index-linux
-del index-macos
-del index-win.exe
-del upload\mtbridge-server-linux
-del upload\mtbridge-server-macos
-del upload\mtbridge-server.exe
-call pkg index.js
-copy index-linux upload\mtbridge-server-linux
-copy index-macos upload\mtbridge-server-macos
-copy index-win.exe upload\mtbridge-server.exe
+npm run dist:clean
+npm run dist:mac:arm64
+npm run dist:mac:x64
+npm run dist:linux:arm64
+npm run dist:linux:x64
+npm run dist:win:arm64
+npm run dist:win:x64
---
+++ build.sh
@@ -0,0 +1,7 @@
+npm run dist:clean
+npm run dist:mac:arm64
+npm run dist:mac:x64
+npm run dist:linux:arm64
+npm run dist:linux:x64
+npm run dist:win:arm64
+npm run dist:win:x64
